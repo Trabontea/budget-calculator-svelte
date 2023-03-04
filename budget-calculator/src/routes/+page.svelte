cb682fd727c4032fc297cbb8fef7dda4415347e6
Trabontea <danielmbotea@gmail.com>
2023-03-04 02:10:13 +0200

    first commit

---
+++ budget-calculator/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Navbar from "../components/Navbar.svelte";
+  import Title from "../components/Title.svelte";
+</script>
+
+<Navbar />
+<div class="container">
+  <a href="/about">About my site</a>
+  
+  <Title title="add expense"/>
+  <Title title="expense list" />
+  <Title />
+
+ </div>

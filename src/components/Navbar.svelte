9979d64a267aeb83edd110309c79e7794dac133a
Trabontea <danielmbotea@gmail.com>
2023-03-29 23:40:45 +0300

    first commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,11 @@
+<nav class="nav">
+  <div class="nav-center">
+    <h1 class="nav-title">
+      Budget Calculator
+    </h1>
+    <button type="button" class="nav-btn">
+      <i class="far fa-plus-square" />
+      <span>Add Item</span>
+    </button>
+  </div>
+</nav> 
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,4 @@
+<div class="container">
+  <h1>Hello and welcome to my site!</h1>
+  <a href="/">Go to home</a>
+</div>
